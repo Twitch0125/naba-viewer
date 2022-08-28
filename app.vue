b2ce0ea8285a28dcb0c534aaebdc04edd831ccbc
Kaleb Ercanbrack <44715857+Twitch0125@users.noreply.github.com>
2022-08-28 16:10:09 -0600

    report a bug link

--- app.vue
+++ app.vue
@@ -4,9 +4,14 @@
   </Head>
   <div class="bg-base-200 flex flex-col min-h-screen text-base">
     <nav
-      class="text-base-content container max-w-2xl mx-auto mt-6 text-sm font-medium uppercase"
+      class="text-base-content container max-w-2xl mx-auto mt-6 space-x-3 text-sm font-medium uppercase"
     >
-      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link class="hover:underline" to="/">Home</nuxt-link>
+      <a
+        class="hover:underline"
+        href="https://github.com/Twitch0125/naba-viewer/issues/new"
+        >Report a bug</a
+      >
     </nav>
     <nuxt-page />
   </div>
