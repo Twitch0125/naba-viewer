b4b84587bb706b70eec804a3bb53fa63d517d3c1
Kaleb Ercanbrack <44715857+Twitch0125@users.noreply.github.com>
2022-08-28 22:27:06 -0600

    card blurb

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,16 @@
       Welcome to NABA Tools!
     </h1>
     <div
-      class="bg-base-100 flex flex-col items-center justify-center w-full max-w-sm py-6 rounded shadow"
+      class="bg-base-100 flex flex-col items-center justify-center w-full max-w-sm p-6 space-y-4 text-center rounded shadow"
     >
-      <nuxt-link
-        class="text-base-content600 mb-2 text-xl font-medium"
-        to="/batting"
+      <nuxt-link class="text-base-content600 text-xl font-medium" to="/batting"
         >Batting Stats Table</nuxt-link
       >
-      <nuxt-link class="btn btn-primary btn-md" to="/batting"> Go </nuxt-link>
+      <p>
+        Upload the batting stat dump from discord and sort by each stat.
+        <b>Work in progress.</b>
+      </p>
+      <nuxt-link class="btn btn-primary btn-md\" to="/batting"> Go </nuxt-link>
     </div>
   </div>
 </template>
