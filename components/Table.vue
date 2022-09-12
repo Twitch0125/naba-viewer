6991021fd6ba7d16b63b675b6dd7bb519b1c2fd0
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2022-09-11 22:20:31 -0600

    fix sort arrows

--- components/Table.vue
+++ components/Table.vue
@@ -74,20 +74,28 @@ const itemsToShow = computed(() => {
                   <th
                     v-for="header in headers"
                     :key="header.name"
-                    class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-3 text-left text-sm font-semibold text-base-content backdrop-blur backdrop-filter first:pl-6 last:pr-6 cursor-pointer"
+                    class="sticky top-0 z-10 border-b uppercase border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-3 text-xs font-medium text-base-content backdrop-blur backdrop-filter first:pl-6 last:pr-6 cursor-pointer"
                     :title="`${header.name}: ${header.description}`"
                     @click="() => setSort(header)"
                   >
-                    {{
-                      header.display === "tbd" ? header.name : header.display
-                    }}
-                    <span v-if="sortBy.value === header.name">
-                      <ArrowUpIcon class="w-5 h-5" v-show="sortBy.ascending" />
-                      <ArrowDownIcon
-                        class="w-5 h-5"
-                        v-show="!sortBy.ascending"
-                      />
-                    </span>
+                    <div class="relative flex items-center">
+                      {{
+                        header.display === "tbd" ? header.name : header.display
+                      }}
+                      <span
+                        class="opacity-70 -mr-4"
+                        v-show="sortBy.value === header.name"
+                      >
+                        <ArrowUpIcon
+                          class="w-4 h-4"
+                          v-show="sortBy.ascending"
+                        />
+                        <ArrowDownIcon
+                          class="w-4 h-4"
+                          v-show="!sortBy.ascending"
+                        />
+                      </span>
+                    </div>
                   </th>
                 </tr>
               </thead>
