33f6e4005958cf2860fca4ce2d6417ec80988faf
Kaleb Ercanbrack <twitcherc@gmail.com>
2022-09-29 17:24:44 -0600

    I just want BaaS that works great with nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+const { client, account } = useAppwrite();
+const { data } = await useAsyncData(async () => {
+  console.log("client");
+  return await account.get();
+});
+watch(data, console.log);
+const getSession = async () => {
+  const res = await account.get();
+  console.log("res", res);
+  return res;
+};
+</script>
 <template>
   <div
     class="container mx-auto flex max-w-2xl flex-col items-center sm:items-start"
@@ -8,6 +20,9 @@
       <!-- <span v-else> Welcome back, {{ client.authStore.model.email }} </span> -->
     </h1>
     <div class="flex gap-2">
+      <button class="btn btn-primary btn-lg" @click="getSession">
+        Get Session
+      </button>
       <div
         class="flex w-full max-w-sm flex-col items-center justify-center space-y-4 rounded bg-base-100 p-6 text-center shadow"
       >
@@ -20,7 +35,7 @@
           Upload the batting stat dump from discord and sort by each stat.
           <b>Work in progress.</b>
         </p>
-        <nuxt-link class="btn-primary btn-md btn" to="/batting"> Go </nuxt-link>
+        <nuxt-link class="btn btn-primary btn-md" to="/batting"> Go </nuxt-link>
       </div>
       <div
         class="flex w-full max-w-sm flex-col items-center justify-center space-y-4 rounded bg-base-100 p-6 text-center shadow"
@@ -33,7 +48,7 @@
         <p>
           <b>Work in progress.</b>
         </p>
-        <nuxt-link class="btn-primary btn-md btn" to="/players"> Go </nuxt-link>
+        <nuxt-link class="btn btn-primary btn-md" to="/players"> Go </nuxt-link>
       </div>
     </div>
   </div>
